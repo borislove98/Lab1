f9bf35c069d4b351aad473e18d52d6fe47df01b8
borislove98 <47853829+borislove98@users.noreply.github.com>
2019-02-21 14:34:50 +0300

    Add files via upload

---
+++ Project1.dpr
@@ -0,0 +1,15 @@
+program Project1;
+
+{$APPTYPE CONSOLE}
+
+uses
+  SysUtils;
+
+begin
+  writeln('Hello, World from Delphi!');
+  writeln('This is the console application');
+  Writeln;
+  Writeln('Press <Enter> to quit.');
+  Readln;
+
+end.
